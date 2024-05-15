32fe9fce07037c5be4a7e642d0fbc35695839eaa
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-05-15 18:32:34 -0300

    novo desafio

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`a valocidade do seu carro é ${vel} km/H`)
+if(vel >= 60){ //condição simples
+    console.log(`você ultrapassou a velocidade permitida, MULTADO`)
+}
+console.log(`dirija com segurança!`)
