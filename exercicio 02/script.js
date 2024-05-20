2f3449c1f4040135686737e2dd434c74c9c6346e
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-05-20 14:39:08 -0300

    Update script.js

--- exercicio 02/script.js
+++ exercicio 02/script.js
@@ -18,15 +18,15 @@ function verificar(){
             img.setAttribute('src', 'bebe-homen.png')
         } else if ( idade < 21){
             // Joven
-            img.setAttribute('src', 'jovem-homen.png')
+            img.setAttribute('src', 'jovem-homem.png')
         } else if (idade < 50) {
             //adulto
-            img.setAttribute('src', 'adulto-homen.png')
+            img.setAttribute('src', 'adulto-homem.png')
         } else {
             //idoso
-            img.setAttribute('src', 'idoso-homen.png')
+            img.setAttribute('src', 'idoso-homem.png')
         }
-    } else if (fsex[2].checked) {
+    } else if (fsex[1].checked) {
         genero = 'Mulher'
         if(idade >= 0 && idade < 10) {
             // Criança
