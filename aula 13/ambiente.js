fdd5d3f5941b9783df3ccc46f07c69c5dba0ecd1
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-05-21 14:27:03 -0300

    novos exercicios

---
+++ aula 13/ambiente.js
@@ -0,0 +1,26 @@
+//função de repetição com o teste logico no final
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 60)
+
+
+//função de repetição com looping e teste logico no começo
+/*var c = 1
+while ( c <= 6) {
+    console.log(`passo ${c}`)
+    c++
+}
+*/
+
+//repetição manual
+/*
+console.log('tudo bem!)
+console.log('tudo bem!)
+console.log('tudo bem!)
+console.log('tudo bem!)
+console.log('tudo bem!)
+console.log('tudo bem!)
+console.log('tudo bem!)
+*/
